0f2e10a6fbd42669484d63103d171e6774339fc5
Ethan Fox <ethan78fox@gmail.com>
2025-11-08 19:34:56 -0500

    configure https

--- load-balancer/main.tf
+++ load-balancer/main.tf
@@ -22,16 +22,47 @@ resource "google_compute_url_map" "backend" {
   default_service = google_compute_backend_service.backend_api.id
 }
 
-# HTTP proxy for frontend (Cloudflare handles SSL)
+# Managed SSL certificates
+resource "google_compute_managed_ssl_certificate" "frontend" {
+  name = "frontend-ssl-cert"
+
+  managed {
+    domains = [var.domain_name]
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "backend" {
+  name = "backend-ssl-cert"
+
+  managed {
+    domains = ["api.${var.domain_name}"]
+  }
+}
+
+# HTTP proxy for frontend
 resource "google_compute_target_http_proxy" "frontend" {
   name    = "frontend-http-proxy"
   url_map = google_compute_url_map.frontend.id
 }
 
-# HTTP proxy for backend (Cloudflare handles SSL)
+# HTTPS proxy for frontend
+resource "google_compute_target_https_proxy" "frontend" {
+  name             = "frontend-https-proxy"
+  url_map          = google_compute_url_map.frontend.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.frontend.id]
+}
+
+# HTTP proxy for backend
 resource "google_compute_target_http_proxy" "backend" {
   name    = "backend-http-proxy"
   url_map = google_compute_url_map.backend.id
+}
+
+# HTTPS proxy for backend
+resource "google_compute_target_https_proxy" "backend" {
+  name             = "backend-https-proxy"
+  url_map          = google_compute_url_map.backend.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.backend.id]
 }
 
 # Forwarding rules for frontend
@@ -44,7 +75,7 @@ resource "google_compute_global_forwarding_rule" "frontend_http" {
 
 resource "google_compute_global_forwarding_rule" "frontend_https" {
   name       = "frontend-https-forwarding-rule"
-  target     = google_compute_target_http_proxy.frontend.id
+  target     = google_compute_target_https_proxy.frontend.id
   port_range = "443"
   ip_address = google_compute_global_address.frontend.address
 }
@@ -59,7 +90,7 @@ resource "google_compute_global_forwarding_rule" "backend_http" {
 
 resource "google_compute_global_forwarding_rule" "backend_https" {
   name       = "backend-https-forwarding-rule"
-  target     = google_compute_target_http_proxy.backend.id
+  target     = google_compute_target_https_proxy.backend.id
   port_range = "443"
   ip_address = google_compute_global_address.backend.address
 }
